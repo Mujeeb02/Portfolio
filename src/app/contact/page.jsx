dd04d8ef6823dbf0a4c6eeed25a49624970d7b07
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2024-08-02 11:28:55 +0530

    contact responsiveness fixed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
                 <form
                     onSubmit={sendEmail}
                     ref={form}
-                    className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+                    className="h-1/2 -mt-8 md:mt-0 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
                 >
                     <span>Dear Mujeeb,</span>
                     <textarea
